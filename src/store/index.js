0a4a2726ed1267c67d517095bc96513699eccc29
罗跃跃 <luoyueyue@luoyueyuedeMacBook-Pro.local>
2019-11-29 15:46:19 +0800

    项目初始化

--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,9 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  state: {},
+  mutations: {},
+  actions: {},
+  //  分模块进行维护，当项目维护的数据越来越多时，有时候就会通过modules来分模块来维护这些数据
+  modules: {}
 })
---
+++ src/views/layout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <!-- 导航栏 -->
+    <!-- 内容容器 -->
+    <!-- 底导航 -->
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
